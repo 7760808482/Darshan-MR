b5e74201802cff13264a4081d1102161f4309b3c
7760808482 <143525866+7760808482@users.noreply.github.com>
2025-10-06 14:14:28 +0530

    Add files via upload

---
+++ darshan2.R
@@ -0,0 +1,22 @@
+num1<-55
+num2<-60
+sum_result<-num1+num2
+difference_result<-num1-num2
+product_result<-num1*num2
+quotient_result<-num1/num2
+remainder_result<-num1%%num2
+cat("Arithmetic Operation:\n")
+cat("Sum:",sum_result,"\n")
+cat("Difference:",difference_result,"\n")
+cat("Product:",product_result,"\n")
+cat("Quotient:",quotient_result,"\n")
+cat("Remainder:",remainder_result,"\n")
+Logical_var<-TRUE
+Logical_var<-FALSE
+and_result<-logical_var1&logical_var2
+or_result<-logical_var1|logical_var2
+not_result<-!logical_var
+cat("Logical Operation:\n")
+cat("AND:",and_result,"\n")
+cat("OR:",or_result,"\n")
+cat("NOT:",not_result,"\n")
---
+++ darshanmr1.R
@@ -0,0 +1,10 @@
+double_var<-3.14
+integer_var<-42L
+logical_var<-TRUE
+complex_var<-1+2i
+character_var<-"Hello,world!"
+cat("Double Variabel:",double_var,"-Type:",typeof(double_var),"\n")
+cat("Integer variabel",integer_var,"-Type",typeof(integer_var),"\n")
+cat("Logical Variabel",logical_var,"-Type",typeof(logical_var),"\n")
+cat("Complex Variabel",complex_var,"-Type",typeof(complex_var),"\n")
+cat("character variabel",character_var,"-Type",typeof(character_var),"\n")

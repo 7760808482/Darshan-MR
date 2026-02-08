ab3419263acdfbca773699422726d815bc70395e
Darshan M R <143525866+7760808482@users.noreply.github.com>
2026-02-08 23:09:01 +0530

    Add files via upload

---
+++ p19.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.data, end=" ")
+        inorder(root.right)
+root = Node(10)
+root.left = Node(20)
+root.right = Node(30)
+print("Inorder Traversal:")
+inorder(root)

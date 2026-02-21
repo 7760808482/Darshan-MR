ca0901f11b17a555b547fac1af4e428ae8292550
Darshan M R <143525866+7760808482@users.noreply.github.com>
2026-02-21 22:30:56 +0530

    Add files via upload

--- factorial.py
+++ factorial.py
@@ -1,18 +1,10 @@
-def factorial(n):
- if n == 0 or n == 1:
-    return 1
- else:
-    result = 1
- for i in range(2, n + 1):
-     result *= i
- return result
-def binomial_coefficient(N, R):
- return factorial(N) // (factorial(R) * factorial(N - R))
-N = int(input("Enter the value of N: "))
-R = int(input("Enter the value of R: "))
-if R > N:
- print("Invalid input. R must be less than or equal to N.")
-else:
- binom_coeff = binomial_coefficient(N, R)
- print(f"Binomial Coefficient C({N}, {R}) = {binom_coeff}")
- 
+'''You have to complete a function named factorial which will return the factorial of the integer N.
+Note: Main code is already implemented, you need to complete the factorial function only.'''
+#Return the factorial of the number N
+def factorial(N):
+    # Your code goes here
+    if N==0 or N==1:
+        return 1
+    else:
+        return(N*factorial(N-1))
+    return
